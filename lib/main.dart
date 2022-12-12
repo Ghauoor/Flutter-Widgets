7c11d53e11cea6ea7876c250ad3dfc3ab5c3eb98
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2022-12-12 22:21:32 +0500

    BottomSheetWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'package:widgets/widgets/bottom_navigation_bar_widget.dart';
+import 'package:widgets/widgets/bottom_sheet_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
           elevation: 0.0,
           title: const Text('This is Widget Learnig Project'),
         ),
-        body: const BottomNavBarWidget(),
+        body: const BottomSheetWidget(),
       ),
     );
   }
---
+++ lib/widgets/bottom_sheet_widget.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+class BottomSheetWidget extends StatefulWidget {
+  const BottomSheetWidget({Key? key}) : super(key: key);
+
+  @override
+  State<BottomSheetWidget> createState() => _BottomSheetWidgetState();
+}
+
+class _BottomSheetWidgetState extends State<BottomSheetWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        child: const Text('Sheet Bar'),
+        onPressed: () {
+          showBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return SizedBox(
+                  height: 400,
+                  child: Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      child: const Text('Close'),
+                    ),
+                  ),
+                );
+              });
+        },
+      ),
+    );
+  }
+}
